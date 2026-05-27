06a151a17c88baad1dfa7e742592e3dae313f8f1
charles <charles@sio.ndlp>
2026-05-27 14:32:53 +0200

    Remplacement

---
+++ src/Controller/PlayerController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class PlayerController extends AbstractController
+{
+    #[Route('/player', name: 'player_index')]
+    public function index(): Response
+    {
+        return new Response ('Salut les SIO !');
+    }
+}
